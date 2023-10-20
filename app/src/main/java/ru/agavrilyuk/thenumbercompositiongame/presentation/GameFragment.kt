2b890682dcedcac4ed58201c270e625cdbd2c4d8
zerocool <one1970zero@mail.ru>
2023-10-20 14:35:01 +0300

    Создание фрагментов и добавление картинок

---
+++ app/src/main/java/ru/agavrilyuk/thenumbercompositiongame/presentation/GameFragment.kt
@@ -0,0 +1,19 @@
+package ru.agavrilyuk.thenumbercompositiongame.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.agavrilyuk.thenumbercompositiongame.R
+
+class GameFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+}
