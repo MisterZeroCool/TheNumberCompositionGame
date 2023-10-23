76127705ed21fde28c16e6dcbec7b33dfc24e874
zerocool <one1970zero@mail.ru>
2023-10-23 20:33:27 +0300

    Создание ViewModel. добавлены методы startGame() chooseAnswer() finishGame() updateProgress() calculatePercentOfRightAnswers(): Int checkAnswer(number: Int) getGameSetting(level: Level) startTimer() generateQuestion() formatTime() finishGame()

--- app/src/main/java/ru/agavrilyuk/thenumbercompositiongame/domain/entity/Question.kt
+++ app/src/main/java/ru/agavrilyuk/thenumbercompositiongame/domain/entity/Question.kt
@@ -1,7 +1,13 @@
 package ru.agavrilyuk.thenumbercompositiongame.domain.entity
 
+import java.io.Serializable
+
 data class Question(
     val sum: Int,
     val visibleNumber: Int,
     val options: List<Int>
-)
+){
+    val rightAnswer: Int
+        get() = sum - visibleNumber
+}
+
